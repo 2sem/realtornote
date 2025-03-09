0ea62812f6a01b03b495380f901cf4e06f2cfb08
leesam <toyboy2@hanmail.net>
2025-03-09 15:22:48 +0900

    refactor: upgrade tuist to 4.38.2

---
+++ Tuist.swift
@@ -0,0 +1,17 @@
+//
+//  Tuist.swift
+//  realtornoteManifests
+//
+//  Created by 영준 이 on 3/9/25.
+//
+
+import ProjectDescription
+
+let tuist = Tuist(
+    project: .tuist(compatibleXcodeVersions: .upToNextMajor("16.0")
+//                    swiftVersion: "",
+//                    plugins: <#T##[PluginLocation]#>,
+//                    generationOptions: <#T##Tuist.GenerationOptions#>,
+//                    installOptions: <#T##Tuist.InstallOptions#>)
+    )
+)
